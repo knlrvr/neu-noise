ffec30cc43ea24085feae16766be5dbefc9310f6
Kane <kanelariviere@outlook.com>
2023-07-13 20:33:06 -0400

    features styling pt. 2

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -19,13 +19,13 @@ const Features = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 border-t-2 border-b-2 border-[#222]">
         <div className="border-b-2 md:border-b-0 md:border-r-2 border-[#222] p-4 relative">
           <span className="text-sm uppercase font-semibold tracking-tight">
-            Maximize SOC efficiency by reducing noisy alerts.
+            Maximize SOC efficiency by <br /> reducing noisy alerts.
           </span>
           <div className="py-12">
             <AiOutlineControl 
               className="text-6xl" />
           </div>
-          <div className="flex flex-col space-y-2 ml-4">
+          <div className="flex flex-col space-y-2 ml-4 mb-32">
             <p className="flex items-start text-sm font-mono">
               <span className="inline-flex"><BsDot className="text-lg" /></span>
               Deprioritize events from benign IPs and common business services.
@@ -39,8 +39,8 @@ const Features = () => {
               Automate workflows and filter noisy alerts across SIEM, SOAR, TIP and other tools.
             </p>
           </div>
-          <div className="flex space-x-4 items-start pt-12 pb-4 text-xs font-mono">
-            <span>20% increased capacity in the SOC.</span>
+          <div className="absolute bottom-2 left-4 flex space-x-4 items-start pt-12 pb-4 text-xs font-mono">
+            <span>20% increased capacity in the <br />SOC.</span>
           </div>
 
           <div className="cursor-pointer group absolute border-2 border-[#222] h-12 w-12 rounded-full -bottom-6 right-3 bg-white flex justify-center items-center">
@@ -51,13 +51,13 @@ const Features = () => {
 
         <div className="border-b-2 md:border-b-0 md:border-r-2 border-[#222] p-4 relative">
           <span className="text-sm uppercase font-semibold tracking-tight">
-            Defend against mass exploitation.
+            Defend against mass <br /> exploitation. <br />
           </span>
-          <div className="pt-[4.5rem] pb-12">
+          <div className="pt-12 pb-12">
             <BiRadar 
               className="text-6xl" />
           </div>
-          <div className="flex flex-col space-y-2 ml-4">
+          <div className="flex flex-col space-y-2 ml-4 mb-32">
             <p className="flex items-start text-sm font-mono">
               <span className="inline-flex"><BsDot className="text-lg" /></span>
               Deprioritize events from benign IPs and common business services.
@@ -71,8 +71,8 @@ const Features = () => {
               Automate workflows and filter noisy alerts across SIEM, SOAR, TIP and other tools.
             </p>
           </div>
-          <div className="flex space-x-4 pt-12 pb-4 text-xs font-mono">
-            <span>Optimize patching & protect during &quot;window of exposure.&quot;</span>
+          <div className="absolute bottom-2 left-4 flex space-x-4 pt-12 pb-4 text-xs font-mono">
+            <span>Optimize patching & protect during <br /> &quot;window of exposure.&quot;</span>
           </div>
 
           <div className="cursor-pointer group absolute border-2 border-[#222] h-12 w-12 rounded-full -bottom-6 right-3 bg-white flex justify-center items-center">
@@ -83,11 +83,25 @@ const Features = () => {
 
         <div className="p-4 relative">
           <span className="text-sm uppercase font-semibold tracking-tight">
-            Contextualize and automate threat hunting.
+            Contextualize and automate <br /> threat hunting.
           </span>
           <div className="py-12">
             <BiTargetLock
               className="text-6xl" />
+          </div>
+          <div className="flex flex-col space-y-2 ml-4 mb-32">
+            <p className="flex items-start text-sm font-mono">
+              <span className="inline-flex"><BsDot className="text-lg" /></span>
+              Enable threat hunters to discover the tactics, techniques, and procedures (TTPs) of 
+              adversaries with attack telemetry. 
+            </p>
+            <p className="flex items-start text-sm font-mono">
+              <span className="inline-flex"><BsDot className="text-lg" /></span>
+              Bolster and automate existing cyber threat investigations with IP metadata.
+            </p>
+          </div>
+          <div className="absolute bottom-2 left-4 flex space-x-4 pt-14 lg:pt-[6.2rem] xl:pt-[6.2rem] pb-4 text-xs font-mono">
+            <span>Gain valuable insight into emerging <br /> threats on the internet.</span>
           </div>
 
           <div className="cursor-pointer group absolute border-2 border-[#222] h-12 w-12 rounded-full -bottom-6 right-3 bg-white flex justify-center items-center">
