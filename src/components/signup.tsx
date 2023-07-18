8674bce6850ba19c7196aaeb1aac6df40ec31589
Kane <kanelariviere@outlook.com>
2023-07-18 08:21:02 -0400

    pointer on sign up

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -10,13 +10,13 @@ const SignUp = () => {
       </div>
 
       <div className="flex justify-evenly py-12">
-        <div className="border-2 border-[#222] flex items-center justify-center py-2 px-4 bg-blue-200 relative group">
+        <div className="border-2 border-[#222] flex items-center justify-center py-2 px-4 bg-blue-200 relative group cursor-pointer">
           <span className="text-center text-xs md:text-sm uppercase font-mono tracking-widest">
             Search For Free
           </span>
           <div className="absolute bg-[#222] h-full w-[101%] -z-10 top-2 md:top-2.5 left-1 md:left-2 group-hover:translate-x-1 group-hover:translate-y-1 transition duration-200"></div>
         </div>
-        <div className="border-2 border-[#222] flex items-center justify-center py-2 px-4 bg-blue-500 group relative group">
+        <div className="border-2 border-[#222] flex items-center justify-center py-2 px-4 bg-blue-500 group relative group cursor-pointer">
           <span className="text-center text-xs md:text-sm uppercase font-mono tracking-widest">
             Schedule A Demo
           </span>
