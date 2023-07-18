0ab110249d2d64202a369f708a4e905730542156
Kane <kanelariviere@outlook.com>
2023-07-18 08:17:04 -0400

    changed Link to div; no reroute

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -11,14 +11,13 @@ const Calculator = () => {
           Check out our ROI Calculator to see how much your organization could
           save by using GreyNoise.
         </p>
-        <Link 
-          href="/"
+        <div
           className="border-2 border-[#222] flex items-center justify-center py-2 px-4 bg-orange-300 relative group">
           <span className="text-center text-xs md:text-sm uppercase font-mono tracking-widest">
             Discover Your Savings
           </span>
           <div className="absolute bg-[#222] h-full w-[101%] -z-10 top-2 md:top-2.5 left-1 md:left-2 group-hover:translate-x-1 group-hover:translate-y-1 transition duration-200"></div>
-        </Link>
+        </div>
       </div>
     </div>
   )
